576f7253ef23b59088ba0aeab999da46de0d1159
jcl <furiiibond@gmail.com>
2020-10-27 08:00:49 +0100

    init

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World this is a test");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
